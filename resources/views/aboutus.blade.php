3781423cc40fadc5349b2cb258449bcaabbe2955
Pabita Pun <pabita.pun23@gmail.com>
2022-09-18 17:49:06 +0545

    changes

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -23,7 +23,7 @@
                 <div class="row h-100 align-items-center py-5">
                     <div class="col-lg-6">
                         <h1 class="display-4" style="color:#D02020; font-size: 60px; font-weight: bolder;">About Us</h1>
-                        <p class="lead text-muted mb-0" style="font-size: 25px; margin-top:20px;">Pursell is an online
+                        <p class="lead text-muted mb-0" style="font-size: 22px; margin-top:20px;">Pursell is an online
                             platform that enables users to list a broad variety of products that users want to sell online
                             which is then visible to everyone accessing the site and other users can search and buy the
                             products they want or need. Also, the sellers can donate the products to their desired
@@ -40,9 +40,9 @@
             <div class="container py-5">
                 <div class="row align-items-center mb-5">
                     <div class="col-lg-6 order-2 order-lg-1">
-                        <h2 class="font-weight-light" style="color:#D02020; font-size: 45px; font-weight: bolder;">Find what
-                            you want easily</h2>
-                        <p class="font-italic text-muted mb-4" style="font-size: 25px; margin-top:20px;">We uses the
+                        <h2 class="font-weight-dark" style="color:#D02020; font-size: 45px; font-weight: bolder;">Find What
+                            You Want Easily</h2>
+                        <p class="lead text-muted mb-4" style="font-size: 22px; margin-top:20px;">We uses the
                             location in order to categorize the items according to the user’s
                             preferences and show the products which are most likely to be picked by the users. Pursell is a
                             perfect stop for the buying, selling, and donating of products online.</p>
@@ -55,10 +55,10 @@
                     <div class="col-lg-5 px-5 mx-auto"><img src="images/donation.jpg" alt=""
                             class="img-fluid mb-4 mb-lg-0"></div>
                     <div class="col-lg-6">
-                        <h2 class="font-weight-light"
+                        <h2 class="font-weight-dark"
                             style="color:#D02020; font-size: 45px; font-weight: bolder; margin-top: 15px;">Extend A Helping
                             Hand</h2>
-                        <p class="font-italic text-muted mb-4" style="font-size: 25px; margin-top:20px;">At Pursell You have
+                        <p class="lead text-muted mb-4" style="font-size: 22px; margin-top:20px;">At Pursell You have
                             the Power to Bring Happiness. Donate stuff to needy people by selecting among the listed
                             organizations on our site.</p>
                     </div>
@@ -77,17 +77,17 @@
                 <div class="row text-center">
                     <!-- Team item-->
                     <div class="col-xl-4 col-sm-6 mb-5">
-                        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="images/bikal.jpg" alt=""
-                                width="150" style="margin-top: 20px;"
+                        <div class="bg-white rounded shadow-sm py-5 px-4">
+                            <img src="images/bikal.jpg" alt="" width="150" style="margin-bottom: 20px;"
                                 class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-                            <h5 class="mb-0">Bikalpa KC</h5>
+                            <h4 class="mb-0 mt-4">Bikalpa KC</h4>
                             <ul class="social mb-0 list-inline mt-3">
                                 <li class="list-inline-item"><a href="https://www.facebook.com/bikku.rocky/"
-                                        class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                                        class="social-link"><i class="fa fa-facebook-f" style="color:#d02020"></i></a></li>
                                 <li class="list-inline-item"><a href="https://www.instagram.com/bikal_kc21/"
-                                        class="social-link"><i class="fa fa-instagram"></i></a></li>
+                                        class="social-link"><i class="fa fa-instagram" style="color:#d02020"></i></a></li>
                                 <li class="list-inline-item"><a href="https://www.linkedin.com/in/bikalpa-kc-505205192/"
-                                        class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                                        class="social-link"><i class="fa fa-linkedin" style="color:#d02020"></i></a></li>
                             </ul>
                         </div>
                     </div>
@@ -96,16 +96,16 @@
                     <!-- Team item-->
                     <div class="col-xl-4 col-sm-6 mb-5">
                         <div class="bg-white rounded shadow-sm py-5 px-4"><img src="images/pabita.jpeg" alt=""
-                                width="150" style="margin-top: 20px;"
+                                width="150" style="margin-bottom: 20px;"
                                 class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-                            <h5 class="mb-0">Pabita Pun</h5>
+                            <h4 class="mb-0 mt-4">Pabita Pun</h4>
                             <ul class="social mb-0 list-inline mt-3">
                                 <li class="list-inline-item"><a href="https://www.facebook.com/pabita.pun.731"
-                                        class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                                        class="social-link"><i class="fa fa-facebook-f" style="color:#d02020"></i></a></li>
                                 <li class="list-inline-item"><a href="https://www.instagram.com/pabita_pun/"
-                                        class="social-link"><i class="fa fa-instagram"></i></a></li>
+                                        class="social-link"><i class="fa fa-instagram" style="color:#d02020"></i></a></li>
                                 <li class="list-inline-item"><a href="https://www.linkedin.com/in/pabita-pun-2390a0193/"
-                                        class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                                        class="social-link"><i class="fa fa-linkedin" style="color:#d02020"></i></a></li>
                             </ul>
                         </div>
                     </div>
@@ -115,18 +115,20 @@
                     <!-- Team item-->
                     <div class="col-xl-4 col-sm-6 mb-5">
                         <div class="bg-white rounded shadow-sm py-5 px-4"><img src="images/rabita.jpeg" alt=""
-                                width="150" style="margin-top: 20px;"
+                                width="150" style="margin-bottom: 20px;"
                                 class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-                            <h5 class="mb-0">Rabita Chhantyal</h5>
+                            <h4 class="mb-0 mt-4">Rabita Chhantyal</h4>
                             <ul class="social mb-0 list-inline mt-3">
                                 <li class="list-inline-item"><a
                                         href="https://www.facebook.com/profile.php?id=100009394778933"
-                                        class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                                        class="social-link"><i class="fa fa-facebook-f" style="color:#d02020"></i></a>
+                                </li>
                                 <li class="list-inline-item"><a href="https://www.instagram.com/rabeeee5/"
-                                        class="social-link"><i class="fa fa-instagram"></i></a></li>
+                                        class="social-link"><i class="fa fa-instagram" style="color:#d02020"></i></a>
+                                </li>
                                 <li class="list-inline-item"><a
                                         href="https://www.linkedin.com/in/rabita-chhantyal-90212419b/"
-                                        class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                                        class="social-link"><i class="fa fa-linkedin" style="color:#d02020"></i></a></li>
                             </ul>
                         </div>
                     </div>
